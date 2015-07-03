7ebad10b8217487fd3e944dd7ab2769ee4d991bf
jtimberman <joshua@housepub.org>
2015-07-02 22:17:39 -0600

    fix broken tests

--- test/integration/default/pester/default.tests.ps1
+++ test/integration/default/pester/default.tests.ps1
@@ -1,25 +1,25 @@
-describe 'housepub-hyperv' do {
+describe 'housepub-hyperv' {
 	It 'should be running Hyper-V Data Exchange Service' {
-		(Get-Service -name).status | Should Be 'Running'
+		(Get-Service -name 'vmickvpexchange').status | Should Be 'Running'
 	}
 
 	It 'should be running Hyper-V Guest Shutdown Service' {
-		(Get-Service -name).status | Should Be 'Running'
+		(Get-Service -name 'vmicshutdown').status | Should Be 'Running'
 	}
 
 	It 'Should be running Hyper-V Heartbeat Service' {
-		(Get-Service -name).status | Should Be 'Running'
+		(Get-Service -name 'vmicheartbeat').status | Should Be 'Running'
 	}
 
 	It 'Should be running Hyper-V Remote Desktop Virtualization Service' {
-		(Get-Service -name).status | Should Be 'Running'
+		(Get-Service -name 'vmicrdv').status | Should Be 'Running'
 	}
 
 	It 'Should be running Hyper-V Time Synchronization Service' {
-		(Get-Service -name).status | Should Be 'Running'
+		(Get-Service -name 'vmictimesync').status | Should Be 'Running'
 	}
 
 	It 'Should be running Hyper-V Volume Shadow Copy Requestor Service' {
-		(Get-Service -name).status | Should Be 'Running'
+		(Get-Service -name 'vmicvss').status | Should Be 'Running'
 	}
 }
